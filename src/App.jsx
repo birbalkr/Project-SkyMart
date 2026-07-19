38d8d2263f7c633b35a3ad862bc5d37012d13600
birbalkr <birbalkr1435@gmail.com>
2026-07-19 22:36:40 +0530

    fix app.jsx error

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,25 @@
-
-import LoginRoutes from "./routes/LoginRoutes"
 import AppRoutes from "./routes/AppRoutes";
-import { Navigate } from "react-router";
-import { useEffect } from "react";
+import LoginRoutes from "./routes/LoginRoutes";
 import Navbar from "./components/Navbar";
 
-
 function App() {
-
   const user = JSON.parse(localStorage.getItem("user") || "null");
   const isLogin = user?.isLogin ?? false;
 
-  useEffect(() => {
-    if (!isLogin && window.location.pathname !== "/login") {
-      window.location.pathname = "/login";
-    }
-  }, [isLogin]);
-
-
   return (
     <>
-      {
-        isLogin ?
-          <div className="bg-black text-white font-sans">
-            <div className="mx-16">
-              <Navbar />
-              <AppRoutes />
-            </div>
+      {isLogin ? (
+        <div className="bg-black text-white font-sans">
+          <div className="mx-16">
+            <Navbar />
+            <AppRoutes />
           </div>
-          : <LoginRoutes />
-      }
+        </div>
+      ) : (
+        <LoginRoutes />
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
